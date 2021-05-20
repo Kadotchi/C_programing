0383f3428a6a6d01a12379c08abd986272362dcc
kador <kador@DESKTOP-LMSH3P1>
2021-05-20 09:30:34 +0900

    画面の追加
    カラム表示を追加した

--- ge_p5/src/ge_p5.c
+++ ge_p5/src/ge_p5.c
@@ -27,8 +27,7 @@ struct ship{
 // void init();//初期化
 void Rule();//ルール説明
 int game_continue();//ゲームを続けるかの選択
-int Input_Rece(int max_num,int min_num);//数値の入力
-int Input_Rece_0(int max_num,int min_num);//数値の入力
+int Input_Rece(int max_num,int min_num,int mode);//数値の入力 modoが0の時は不正な入力は0を返す
 
 /* 月面着陸ゲーム */
 void month_display(struct ship *landing_ship);//表示
@@ -59,36 +58,8 @@ int main(void){
 }
 
 
-int breaks_find(int breaks){//ブレーキの操作
-    int flag;//ブレーキをどうするの判断；
-    printf("2:強く　1:弱く　0:そのまま\n");
-    flag=Input_Rece_0(2,0);//数値の入力
-    if(flag==2){
-        if(breaks==UNIT_MAXIMUM) return 0; //ブレーキがMAXだった場合強くできない
-        /* ブレーキを強く */
-        return 1;
-    }else if(flag==1){
-        if(breaks==0) return 0;//ブレーキが0だった場合弱くできない
-        /* ブレーキを弱く */
-        return -1;
-    }else return 0;
-}
-int speed_find(int speed,int breaks){//速度を求める
-    int next_speed;
-    next_speed=speed-breaks;
-    return next_speed;
-}
-int distance_find(int speed,int distance){//距離を求める
-    int next_distance;
-    next_distance=distance-(speed/C);
-    return next_distance;
-}
 
-
-void display_end(int time,int distance){//ゲーム終了時の表示
-    printf("%d 秒　駅まで%dmで停止しました。\n",time,distance);
-}
-int Input_Rece(int max_num,int min_num){//数値の入力
+int Input_Rece(int max_num,int min_num,int mode){//数値の入力 modeが0の時は不正な入力は0を返す
 	char str[BUF];    //入力バッファ
 	int i;            //ループカウンタ
 	int flag;    //繰り返しカウンタ
@@ -112,10 +83,12 @@ int Input_Rece(int max_num,int min_num){//数値の入力
 		for (i = 0; i < len-1; i++) //データサイズ文取得
 				{
 			if (!isdigit(str[i])) {
+				if(mode){
 				printf(">>ERROR:入力に文字が含まれています。もう一度入力してください。\n");
 				fflush(stdout);
 				flag = 1;
 				break;
+				}else return 0;
 			}
 		}
 		if (flag)
@@ -126,13 +99,17 @@ int Input_Rece(int max_num,int min_num){//数値の入力
 //		printf("入力された数値：%d\n", num);
 		fflush(stdout);
         if(num>max_num){
+        	if(mode){
             printf(">>ERROR:上限を超えています。\n");
             continue;
+        	}else return 0;
         }
 
         if(num<min_num){
+        	if(mode){
             printf(">>ERROR:%dより大きい数値を入力してください。\n",min_num);
             continue;
+        	}else return 0;
         }
 		if (flag)
 			continue;
@@ -146,82 +123,34 @@ int Input_Rece(int max_num,int min_num){//数値の入力
 int game_continue(){//ゲームを続けるかの選択
     int flag;
     printf("ゲームを続けますか？\n2:はい：1:いいえ");
-    flag=Input_Rece(2,1);//数値の入力
+    flag=Input_Rece(2,1,1);//数値の入力
     if(flag==2) return 1;
     else return 0;
 }
 
-/* 不正な入力は0を返すように変更 */
-int Input_Rece_0(int max_num,int min_num){//数値の入力
-	char str[BUF];    //入力バッファ
-	int i;            //ループカウンタ
-	int flag;    //繰り返しカウンタ
-	int len;          //入力データサイズ
-	int num;          //入力された数値
-
-	while (1) {
-		flag = 0; //フラグの初期化
-//		printf("\n0から%dまでの数字を入力してください。：", max_num);
-		fflush(stdout);
-		fgets(str, sizeof(str), stdin); //ユーザからの入力受付
-		len = strlen(str); //データサイズ取得
-		str[BUF - 2] = '\n';
-		str[BUF - 1] = '\0';
-        if(len<BUF){
-            str[len + 1] = '\n';
-		    str[len] = '\0';
-        }
-
-		//入力の数値に文字が入力されてた場合
-		for (i = 0; i < len-1; i++) //データサイズ文取得
-				{
-			if (!isdigit(str[i])) {
-				printf(">>ERROR:入力に文字が含まれています。もう一度入力してください。\n");
-				fflush(stdout);
-				flag = 1;
-				return 0;
-			}
-		}
-		if (flag)
-			continue;
-
-		/*入力された文字列を数列に代入*/
-		num = atoi(&str[0]);
-//		printf("入力された数値：%d\n", num);
-		fflush(stdout);
-        if(num>max_num){
-            printf(">>ERROR:上限を超えています。\n");
-            return 0;
-        }
-
-        if(num<min_num){
-            printf(">>ERROR:%dより大きい数値を入力してください。\n",min_num);
-            return 0;
-        }
-		if (flag)
-			continue;
-		break;
-	}
-    return num;
-	puts(str);
-	fflush(stdout);
-
-}
 
 void month_display(struct ship *landing_ship){//表示
-//    int i;//ループ制御
-//    int train;
-//    train=distance/DISPLAY_MAFNIFICATION;
-//    if(speed==0||distance<-10){
-//        printf("[停止中]\n");
-//    }else printf("[走行中]\n");
-//    printf("|");
-//    for(i=0;i<INITIAL_ALTITUDE/DISPLAY_MAFNIFICATION;i++){
-//        if(i==train){
-//            printf("□□□");
-//            i += 2;
-//        }else printf("_");
-//    }
+    int i;//ループ制御
+    int train;
+    train=landing_ship->altitude/DISPLAY_MAFNIFICATION;
+    if(landing_ship->speed==0||landing_ship->altitude<-10){
+        printf("[停止中]\n");
+    }else printf("[走行中]\n");
+    printf("|");
+
+	if(landing_ship->altitude<=0){
+		printf("□□");
+		for(i=0;i<INITIAL_ALTITUDE/DISPLAY_MAFNIFICATION;i++){
+			printf("_");
+		}
+	}else{
+		for(i=0;i<INITIAL_ALTITUDE/DISPLAY_MAFNIFICATION;i++){
+			if(i==train){
+				printf("□□□");
+				i += 2;
+			}else printf("_");
+		}
+    }
     printf("\n速度：%.2fm/s 現在の高度：%.2fm 残燃料：%d (max%d)\n",landing_ship->speed,landing_ship->altitude,landing_ship->holding_fuel,UNIT_MAXIMUM);
     //月面着陸していた場合
     if(landing_ship->f_landing==0){
@@ -231,12 +160,6 @@ void month_display(struct ship *landing_ship){//表示
     		printf("月面着陸失敗");
     	}
     }
-}
-
-int month_speed_find(int speed,int breaks){//速度を求める
-    int next_speed;
-    next_speed=speed+MONTH_LANDING_SPEED-breaks*LANDING_VALUE;
-    return next_speed;
 }
 
 void Rule() {
@@ -259,7 +182,7 @@ void landing(struct ship *landing_ship){
 
 	//燃料の受付
 	printf("投下する燃料を入力してください。:");
-	fuel=Input_Rece_0(landing_ship->holding_fuel,0);
+	fuel=Input_Rece(landing_ship->holding_fuel,0,0);
 	//所持燃料から今回の燃料分を引く
 	landing_ship->holding_fuel -= fuel;
 	//変化前の速度を格納
